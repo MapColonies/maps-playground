9d17bb768dc2ad73fb1857652b3a40cff0a8d28c
shimoncohen <shimoncohen655@gmail.com>
2026-06-11 17:28:39 +0300

    fix: cesium examples use wrong attributes and types

--- examples/cesium-wmts/cesium.js
+++ examples/cesium-wmts/cesium.js
@@ -4,18 +4,20 @@ import { fetchWmtsTileTemplate } from './utils/wmts-utils.js';
 
 fetchWmtsTileTemplate(PRODUCT_ID, PRODUCT_TYPE, LAYER_IMAGE_FORMAT).then(({ template, name }) => {
 	new Cesium.Viewer('cesiumContainer', {
-		imageryProvider: new Cesium.WebMapTileServiceImageryProvider({
-			url: new Cesium.Resource({
-				url: template,
-				queryParameters: {
-					token: TOKEN
-				}
-			}),
-			layer: name,
-			style: 'default',
-			format: LAYER_IMAGE_FORMAT,
-			tileMatrixSetID: 'WorldCRS84',
-			tilingScheme: new Cesium.GeographicTilingScheme()
-		})
+		baseLayer: new Cesium.ImageryLayer(
+			new Cesium.WebMapTileServiceImageryProvider({
+				url: new Cesium.Resource({
+					url: template,
+					queryParameters: {
+						token: TOKEN
+					}
+				}),
+				layer: name,
+				style: 'default',
+				format: LAYER_IMAGE_FORMAT,
+				tileMatrixSetID: 'WorldCRS84',
+				tilingScheme: new Cesium.GeographicTilingScheme()
+			})
+		)
 	});
 });
